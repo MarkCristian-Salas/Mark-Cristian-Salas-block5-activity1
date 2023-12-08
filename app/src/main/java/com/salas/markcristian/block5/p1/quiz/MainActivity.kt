ffaed49f883fad49c8075f1d4a5c41a4c84ab70e
MarkCristian-Salas <148409701+MarkCristian-Salas@users.noreply.github.com>
2023-12-09 00:13:11 +0800

    Adding sample button

--- app/src/main/java/com/salas/markcristian/block5/p1/quiz/MainActivity.kt
+++ app/src/main/java/com/salas/markcristian/block5/p1/quiz/MainActivity.kt
@@ -7,5 +7,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Add your code here for initializing components, setting up listeners, etc.
+
+        // Example: Set up a button click listener
+        val myButton = findViewById<Button>(R.id.myButton)
+        myButton.setOnClickListener {
+            // Handle button click
+        }
     }
+
+    // Add more methods and components as needed
 }
